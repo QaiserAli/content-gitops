10ffd40adb37420cbaf7bf7ba86566a06376befc
QaiserAli <qaiserali.bangash@gmail.com>
2020-08-06 12:11:17 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello and Welcome to GitOps world!"
+    return "Hello and Welcome to GitOps world. This change is done in feature branch!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
